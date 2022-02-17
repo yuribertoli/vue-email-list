ad78ba651cf7b975fec4d3bf029120720fda08bc
yuribertoli <yuribertoli@yahoo.it>
2022-02-17 15:05:57 +0100

    prima parte

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const app = new Vue ({
+
     el: "#root",
+
     data: {
-
+        emails: []
     },
-    methods: {
 
-    },
     mounted() {
-        
+
+        for (let i=0; i<10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((risposta) => {
+
+                this.emails.push(risposta.data.response);
+            });
+        }
+
+        console.log(this.emails);
+ 
     }
 });
+
